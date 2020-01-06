d2171557e4a92968be5e16ce740e8a5c98bdd8d2
Michael Smith <michael.smith@puppet.com>
2020-01-06 10:57:53 -0800

    Upgrade Helm to v3, remove unneeded parameters
    Helm 3 removed `--recreate-pods` and recommends using a deployment
    instead as noted in
    https://helm.sh/docs/howto/charts_tips_and_tricks/#automatically-roll-deployments.

--- helm/step.sh
+++ helm/step.sh
@@ -7,16 +7,9 @@ CLUSTER=$(ni get -p {.cluster.name})
 
 KUBECONFIG=/workspace/${CLUSTER}/kubeconfig
 
-TLS_OPTIONS=
-CREDENTIALS=$(ni get -p {.credentials})
-if [ -n "${CREDENTIALS}" ]; then
-    ni credentials config -d $(helm home)
-    TLS_OPTIONS="--tls --tls-verify"
-fi
-
 COMMAND=$(ni get -p {.command})
 ARGS=$(ni get -p {.args})
 
 helm init --client-only
-helm ${COMMAND} ${ARGS} ${TLS_OPTIONS} \
+helm ${COMMAND} ${ARGS} \
     --namespace ${NS} --kubeconfig ${KUBECONFIG}
